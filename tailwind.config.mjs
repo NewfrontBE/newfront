1a2c49365e9b00c157e60a6f2faec563fa60f110
NewfrontBE <nick@newfront.be>
2024-11-29 16:32:57 +0100

    import fonts, homepage wip

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/flowbite/**/*.js'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				'slide-left': {
+				  from: { transform: 'translateX(0)' },
+				  to: { transform: 'translateX(-100%)' },
+				},
+			  },
+			  animation: {
+				'slide-left-infinite': 'slide-left 30s linear infinite',
+			  },
+		},
 	},
 	plugins: [
 		require('flowbite/plugin')
